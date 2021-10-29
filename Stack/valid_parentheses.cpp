190970cc01b23a55973910e1a199d7b78930eb6e
Soumya Prabha Maiti <soumyaprabhamaiti2001@gmail.com>
2021-10-29 23:56:22 +0530

    Modified valid parentheses

--- Stack/valid_parentheses.cpp
+++ Stack/valid_parentheses.cpp
@@ -11,7 +11,7 @@ public:
         {
             if(s[i]=='(' || s[i]=='{' || s[i]=='[')
                 stk.push(s[i]);
-            else
+            else if(s[i]==')' || s[i]=='}' || s[i]==']')
             {
                 if(stk.empty())
                     return false;
